1939f2803896ba4281118bb4d8ac0791aa09511e
Nico Nize <nicola.dauria@studio.unibo.it>
2021-02-21 22:44:08 +0100

    ReadFile First Version

--- src/main/java/model/DataChartsImpl.java
+++ src/main/java/model/DataChartsImpl.java
@@ -3,6 +3,12 @@ package model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -15,8 +21,9 @@ import org.knowm.xchart.XYChart;
 public class DataChartsImpl implements DataCharts {
      
     private final static Integer DAY = 1;
-    //private static final String SEP = System.getProperties().getProperty(File.pathSeparator);
-    
+    private static final String SEP = File.separator;
+    private static final String FILE = System.getProperty("user.home") + SEP + "Documenti" + SEP + "ProvaFile.txt";
+            
     public List<Double> buildChartsFromData(LocalDate dateStart, LocalDate dateEnd, Integer choose) {       
         try {
                 if(choose.equals(DatiDaVisualizzareEnum.TEMPOLAVORO.getIndex())) {
@@ -35,18 +42,20 @@ public class DataChartsImpl implements DataCharts {
      
     //Qui devi implementare delle query, conta i giorni dall'inizio alla fine e per ogni giorno assegna la sua entrata,
     //aggiungendola ad una lista che verrà aggiunta al grafico
+    
     private List<Double> getEntrate(LocalDate dateStart, LocalDate dateEnd){
         
         List<Double> entrateList = new ArrayList<>();
-        Double money = 13.70;
+        Double money = 0.0;
         LocalDate auxDate = dateStart;
-        entrateList.add(money);
-        while(!auxDate.isEqual(dateEnd)) {
-            auxDate = auxDate.plusDays(DataChartsImpl.DAY);
-            money++;
-            entrateList.add(money);
+        String line = null;
+        try(BufferedReader reader = new BufferedReader(new FileReader(DataChartsImpl.FILE))){
+            while( (line = reader.readLine()) != null) {
+                entrateList.add(Double.parseDouble(line.substring(line.indexOf("Entrate: ")+9 )));
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
         }
-        
         return entrateList;
     }
     
@@ -54,19 +63,28 @@ public class DataChartsImpl implements DataCharts {
         
         List<Double> lavoroList = new ArrayList<>();
         LocalDate auxDate = dateStart;
-        Double job = 8.0;
-        lavoroList.add(job);
-        
-        while(!auxDate.isEqual(dateEnd)) {
+        Double job = 0.0;
+        String line = null;
+        try(BufferedReader reader = new BufferedReader(new FileReader(DataChartsImpl.FILE))){
+            while( (line = reader.readLine()) != null) {
+                //if(auxDate.equals(LocalDate.parse((line.substring(line.indexOf("Date: "), line.indexOf(" Minuti:")))))) {
+                    lavoroList.add(Double.parseDouble(line.substring(line.indexOf("Minuti: ")+8, line.indexOf(" Entrate: "))));
+                //}
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        /*while(!auxDate.isEqual(dateEnd)) {
             auxDate = auxDate.plusDays(DataChartsImpl.DAY);
             job++;
             lavoroList.add(job);
-        }
+        }*/
         return lavoroList;
     }
     
     //Siccome ad ogni giorno corrisponde un tipo di dato che bisogna tracciare è necessario quindi calcolarsi ogni giorno fino allda dateEnd
     // per poterli poi associare al tipo di dato nel rispettivo giorno
+    
     public List<Date> getDaysDate(final LocalDate dateStart, final LocalDate dateEnd){
         List<Date> successiveDate = new ArrayList<>();    //necessita di convertire da localDate a date perché XYChart non accetta LocalDate;
         LocalDate auxDate = dateStart;
@@ -87,11 +105,10 @@ public class DataChartsImpl implements DataCharts {
                date = sdf.parse(new String(auxDate.toString()));
                successiveDate.add(date);
            } catch (ParseException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+             }    
         }
-          
-        }
+        
         return successiveDate;
     }
 
@@ -103,7 +120,6 @@ public class DataChartsImpl implements DataCharts {
         list.addAll(chart.getSeriesMap().keySet().stream().collect(Collectors.toList()));
         list.remove(list.size()-1);
         chart.getSeriesMap().keySet().retainAll(list);
-    
     }
-
+   
 }
