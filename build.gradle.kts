7f1ad689b57c59e89df75cd2b2a03ee4077fe7de
Roberto Casadei <robyonrails@gmail.com>
2021-03-01 15:07:53 +0100

    Gradle reporting stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@ plugins {
      * The runnable jar will be found in build/libs/projectname-all.jar
      */
     id("com.github.johnrengelman.shadow") version "5.2.0"
+    checkstyle
+    pmd
+    id("de.aaschmid.cpd")
+    id("com.github.spotbugs")
+    `build-dashboard`
 }
 repositories {
     jcenter() // Contains the whole Maven Central + other stuff
@@ -27,7 +32,7 @@ dependencies {
     implementation("com.google.guava:guava:28.1-jre")
     implementation("com.github.lgooddatepicker:LGoodDatePicker:11.0.0")
     implementation("org.knowm.xchart:xchart:3.6.5")
-    
+
     // JUnit API and testing engine
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
@@ -44,4 +49,48 @@ tasks.withType<JavaCompile> {
 
 application {
     mainClassName = "application.Main"
+}
+
+spotbugs {
+    setEffort("max")
+    setReportLevel("low")
+    showProgress.set(true)
+    val excludeFile = File("${project.rootProject.projectDir}/config/spotbugs/excludes.xml")
+    if (excludeFile.exists()) {
+        excludeFilter.set(excludeFile)
+    }
+}
+
+tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
+    ignoreFailures = true
+    reports {
+        create("html") {
+            enabled = true
+        }
+    }
+}
+
+pmd {
+    ruleSets = listOf()
+    ruleSetConfig = resources.text.fromFile("${project.rootProject.projectDir}/config/pmd/pmd.xml")
+    isIgnoreFailures = true
+}
+
+cpd {
+    isIgnoreFailures = true
+}
+
+tasks.withType<Cpd> {
+    reports {
+        xml.setEnabled(false)
+        text.setEnabled(true)
+    }
+    language = "java"
+    minimumTokenCount = 50
+    ignoreFailures = true
+    source = sourceSets["main"].allJava
+}
+
+checkstyle {
+    isIgnoreFailures = true
 }
